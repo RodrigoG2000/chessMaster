261c8fe5ed375de90f959d3d7e7299a161309e01
Rodrigo <rodgp2019@gmail.com>
2024-05-27 20:43:42 -0600

    Inicializando el proyecto de Spring Boot

---
+++ src/main/java/com/chessMaster/chessMasterWeb/ChessMasterWebApplication.java
@@ -0,0 +1,13 @@
+package com.chessMaster.chessMasterWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessMasterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessMasterWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chessMaster/chessMasterWeb/ChessMasterWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessMaster.chessMasterWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessMasterWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
