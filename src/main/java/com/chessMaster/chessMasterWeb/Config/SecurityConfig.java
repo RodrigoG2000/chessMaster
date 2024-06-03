16d51bc11de02998f0499be240e77ae64c6c2c8a
RodrigoG2000 <rodgp2019@gmail.com>
2024-06-02 23:34:13 -0600

    Refresh
    Changes in the project for Security Spring

---
+++ src/main/java/com/chessMaster/chessMasterWeb/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.chessMaster.chessMasterWeb.Config;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf ->
+                        csrf.disable())
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults()).build();
+    }
+}
